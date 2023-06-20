0e91991b165e55f81e4e7e303224cef01225870a
Egor Krechuniak <egorkrecunak@gmail.com>
2023-06-20 15:10:46 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,86 +1,19 @@
+
 import React from 'react';
 import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
 import transactions from '../data/transactions.json';
-
-const getRandomColor = () => {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
-
-const Statistics = ({ title, stats }) => {
-  return (
-    <section className="statistics">
-      {title && <h2 className="title">{title}</h2>}
-      <ul className="stat-list">
-        {stats.map((stat) => (
-          <li
-            key={stat.id}
-            className="item"
-            style={{ backgroundColor: getRandomColor() }}
-          >
-            <span className="label">{stat.label}</span>
-            <span className="percentage">{stat.percentage}%</span>
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
-};
-
-const FriendList = ({ friends }) => {
-  return (
-    <ul className="friend-list">
-      {friends.map((friend) => (
-        <li className="item" key={friend.id}>
-          <span className={`status ${friend.isOnline ? 'online' : 'offline'}`}></span>
-          <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
-          <p className="name">{friend.name}</p>
-        </li>
-      ))}
-    </ul>
-  );
-};
-
-const TransactionHistory = ({ items }) => {
-  return (
-    <table className="transaction-history">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {items.map((transaction) => (
-          <tr key={transaction.id}>
-            <td>{transaction.type}</td>
-            <td>{transaction.amount}</td>
-            <td>{transaction.currency}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
+import Statistics from './Statistics';
+import FriendList from './FriendList';
+import TransactionHistory from './TransactionHistory';
 
 const App = () => {
   return (
     <div>
       <div className="profile">
         <div className="description">
-          <img
-            src={user.avatar}
-            alt="User avatar"
-            className="avatar"
-          />
+          <img src={user.avatar} alt="User avatar" className="avatar" />
           <p className="name">{user.username}</p>
           <p className="tag">@{user.tag}</p>
           <p className="location">{user.location}</p>
